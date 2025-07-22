05386e77bd1738f3a46b33d5c8cdea37cb612453
Alex Li <li.4.alex@gmail.com>
2025-07-21 19:44:55 -0700

    Selected country logged to console in App

---
+++ src/App.jsx
@@ -0,0 +1,126 @@
+'use client';
+import React, { useState, useEffect } from "react";
+import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import SelectCountry from "./components/SelectCountry";
+
+ModuleRegistry.registerModules([AllCommunityModule]);
+
+let country = "US";
+
+const directorFilterParams = {
+  textFormatter: (r) => {
+    if (r == null) return null;
+    return r
+      .toLowerCase()
+      .replace(/[àáâãäå]/g, "a")
+      .replace(/æ/g, "ae")
+      .replace(/ç/g, "c")
+      .replace(/[èéêë]/g, "e")
+      .replace(/[ìíîï]/g, "i")
+      .replace(/ñ/g, "n")
+      .replace(/[òóôõö]/g, "o")
+      .replace(/œ/g, "oe")
+      .replace(/[ùúûü]/g, "u")
+      .replace(/[ýÿ]/g, "y");
+  },
+};
+
+export default function App() {
+  // const gridRef = useRef(null);
+  // const containerStyle = useMemo(() => ({ width: "100%", height: "100%" }), []);
+  // const gridStyle = useMemo(() => ({ height: "100%", width: "100%" }), []);
+  const [rowData, setRowData] = useState([]);
+    // Column Definitions: Defines & controls grid columns.
+  const [colDefs, setColDefs] = useState([
+    { field: "Pos", headerName: "2025", maxWidth: 70 },
+    { field: "2024" },
+    { field: "2023" },
+    { field: "Title" },
+    { field: "Director",
+      filter: true,
+      filterParams: directorFilterParams},
+    { field: "Year", filter: true},
+    { field: "Country", filter: true},
+    { field: "Length", filter: true},
+    { field: "Genre", filter: true},
+    { field: "Colour", filter: true},
+    { field: "Media Type", filter: true},
+    { field: "Release Date", filter: true},
+    { field: "Providers.results.US.link", headerName: "TMDB Link"},
+    { headerName: "Free",
+      // filter: providerFilter,
+      filter: true,
+      valueGetter: function (params) {
+        const providerData = params.data.Providers.results.US.free;
+        return retrieveProviders(providerData);
+      }
+    },
+    { headerName: "Flat Rate (Subscription)",
+      // filter: providerFilter,
+      filter: true,
+      valueGetter: function (params) {
+        const providerData = params.data.Providers.results.US.flatrate;
+        return retrieveProviders(providerData);
+      }
+    },
+    { headerName: "Buy",
+      // filter: providerFilter,
+      filter: true,
+      valueGetter: function (params) {
+        const providerData = params.data.Providers.results.US.buy;
+        return retrieveProviders(providerData);
+      }
+    },
+    { headerName: "Rent",
+      // filter: providerFilter,
+      filter: true,
+      valueGetter: function (params) {
+        const providerData = params.data.Providers.results.US.rent;
+        return retrieveProviders(providerData);
+      }
+    }
+  ]);
+
+  function retrieveProviders(providerData) {
+    if (providerData) {
+      let result = "";
+      for (let i = 0; i < providerData.length; i++) {
+        result += providerData[i].provider_name + ", ";
+      }
+      return result.slice(0, result.length - 2);
+    }
+  };
+
+  useEffect(() => {
+    fetch("./script/tmdb_final_10_us.json")
+    .then(result => result.json())
+    .then(rowData => setRowData(rowData));
+    }, []);
+
+  const defaultColDef = {
+    flex: 1,
+  };
+
+  // const [selected, setSelected] = useState("");
+
+  const handleSelect = (country) => {
+    console.log(country);
+  }
+
+  return (
+    <div style={{ width: "1400px", height: "500px" }}>
+      <SelectCountry onSelect={handleSelect} />
+      <AgGridReact
+        // ref={gridRef}
+        rowData={rowData}
+        columnDefs={colDefs}
+        defaultColDef={defaultColDef}
+        pagination={true}
+        // isExternalFilterPresent={isExternalFilterPresent}
+        // doesExternalFilterPass={true}
+        // onGridReady={onGridReady}
+      />
+    </div>
+  );
+}
---
+++ src/components/SelectCountry.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import ReactFlagsSelect from "react-flags-select";
+
+
+function SelectCountry(props) {
+    const [selected, setSelected] = useState("");
+
+    const handleSelect = (code) => {
+        props.onSelect(code);
+    }
+
+    return (
+        <ReactFlagsSelect
+            selected={selected}
+            onSelect={(code) => {
+                setSelected(code);
+                handleSelect(code);
+            }}
+            className="flag"
+            searchable
+        />
+    );
+};
+
+export default SelectCountry;
