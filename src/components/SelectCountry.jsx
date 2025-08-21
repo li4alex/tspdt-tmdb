e7e524d802113e257913a3d3882ba82c084a279f
Alex Li <li.4.alex@gmail.com>
2025-08-20 17:21:03 -0700

    SelectCountry tabs to 2 spaces

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -2,23 +2,23 @@ import React, { useState } from "react";
 import ReactFlagsSelect from "react-flags-select";
 
 function SelectCountry(props) {
-    const [selected, setSelected] = useState("US");
+  const [selected, setSelected] = useState("US");
 
-    const handleSelect = (code) => {
-        props.onSelect(code);
-    }
+  const handleSelect = (code) => {
+    props.onSelect(code);
+  }
 
-    return (
-        <ReactFlagsSelect
-            selected={selected}
-            onSelect={(code) => {
-                setSelected(code);
-                handleSelect(code);
-            }}
-            className="flag"
-            searchable
-        />
-    );
+  return (
+    <ReactFlagsSelect
+      selected={selected}
+      onSelect={(code) => {
+        setSelected(code);
+        handleSelect(code);
+      }}
+      className="flag"
+      searchable
+    />
+  );
 };
 
 export default SelectCountry;
