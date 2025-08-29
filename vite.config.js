cfd29c2f6b08aa65d442fee9cbfd363e011be5c8
Alex Li <li.4.alex@gmail.com>
2025-08-28 21:31:54 -0700

    Change base to full URL

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/tspdt-tmdb/",
+  base: "https://li4alex.github.io/tspdt-tmdb/",
   plugins: [react()],
 })
