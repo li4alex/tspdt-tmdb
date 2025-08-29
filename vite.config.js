4f0b6e219adf3edca6cbc26fe4cfebb41db3e148
Alex Li <li.4.alex@gmail.com>
2025-08-28 21:40:09 -0700

    Base set back to repo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://li4alex.github.io/tspdt-tmdb/",
+  base: "/tspdt-tmdb/",
   plugins: [react()],
 })
