66b7c891d917e0e7f294fd6cfec305e09b3021ec
Alex Li <li4alex@students.cs.ubc.ca>
2025-09-01 17:10:20 -0700

    Revert to including contact page, removed github pages config

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,12 @@ import ContactForm from "/src/components/ContactForm/ContactForm";
 
 const App = () => (
   <div>
-    {/* <Switch>
+    <Switch>
       <Route path="/" component={TablePage} />
       <Route path="/contact" component={ContactForm} />
       <Route>404: No such page!</Route>
-    </Switch> */}
+    </Switch>
     {/* <TablePage /> */}
-    <div>
-      <h1>Test</h1>
-    </div>
   </div>
 );
 
