3db259d8d167f742b1cccd1004f7d89a89f2457e
Alex Li <li4alex@students.cs.ubc.ca>
2025-09-08 20:53:59 -0700

    useFetchJson typo

--- utils/UseFetchJson.jsx
+++ utils/UseFetchJson.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export default function useFetchJso(url, limit) {
+export default function useFetchJson(url, limit) {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
   useEffect(() => {
