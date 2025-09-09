7dafe2f9fb6278cfab20ecf6ac972a6ad95dcbc8
Alex Li <li4alex@students.cs.ubc.ca>
2025-09-08 20:05:19 -0700

    New daily json

--- src/components/TablePage.jsx
+++ src/components/TablePage.jsx
@@ -32,7 +32,7 @@ const directorFilterParams = {
 };
 
 const TablePage = () => {
-  const { data, loading } = useFetchJson("/tmdb_2025-09-05.json");
+  const { data, loading } = useFetchJson("/tmdb_2025-09-08.json");
   const gridRef = useRef(null);
   const windowWidth = useRef(window.innerWidth);
   const columnDefinitions = () => {
